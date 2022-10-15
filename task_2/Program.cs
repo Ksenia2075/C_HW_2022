b915d723393a4c9ef6e84e8cf40856f834483173
KseniaAk <ksu909090@yandex.ru>
2022-10-15 23:10:44 +0300

    Находим минимально и максимальное число

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,3 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.Write("Максимально число ");
+    Console.WriteLine(numberA);
+    Console.Write("Минимальное число ");
+    Console.Write(numberB);
+}
+
+if(numberA < numberB)
+{
+    Console.Write("Максимально число ");
+    Console.WriteLine(numberB);
+    Console.Write("Минимальное число ");
+    Console.Write(numberA);
+}
+
+if(numberA == numberB)
+{
+    Console.WriteLine("Введеные вами числа равны");
+}
 
 
