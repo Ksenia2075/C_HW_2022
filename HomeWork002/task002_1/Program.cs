21ecf369dd0646ebf24871ba0da2191d89f5bd20
KseniaAk <ksu909090@yandex.ru>
2022-10-26 18:09:19 +0300

    определяем выходной

---
+++ HomeWork002/task002_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int[] array = {1, 2, 3, 4, 5, 6, 7};
+if ( number > array[4])
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Рабочий день");
+}
