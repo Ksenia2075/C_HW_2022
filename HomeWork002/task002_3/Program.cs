988ff7278fc050a9617fa82db409068dd365126f
KseniaAk <ksu909090@yandex.ru>
2022-10-26 21:56:32 +0300

    показать диапазон возможных координат

---
+++ HomeWork002/task002_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Clear();
+Console.WriteLine("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+if (quarter == 1)
+{
+    Console.WriteLine("Диапазон координат x > 0, y < 0");
+}
+if (quarter == 2)
+{
+    Console.WriteLine("Диапазон координат x > 0, y > 0");
+}
+if (quarter == 3)
+{
+    Console.WriteLine("Диапазон координат x < 0, y < 0");
+}
+if (quarter == 4)
+{
+    Console.WriteLine("Диапазон координат x < 0, y > 0");
+}
+else
+{
+    Console.WriteLine("Вы ввели не корректное число");
+}
