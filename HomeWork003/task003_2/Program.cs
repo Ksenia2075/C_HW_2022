6f186e0aa200b6bbe21a8896edb98367122758ac
KseniaAk <ksu909090@yandex.ru>
2022-10-29 20:14:32 +0300

    принимаем трехзначное число и выводим третье

---
+++ HomeWork003/task003_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num / 100 > 0)
+{
+    Console.WriteLine($"третья цифра {num % 10}");
+}
+else
+{
+    Console.WriteLine("третей цифры нет");
+}
