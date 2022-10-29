c70d5b3543f9f2c334b10b72b30cf93082752b97
KseniaAk <ksu909090@yandex.ru>
2022-10-29 19:56:49 +0300

    принимаем трехзначное число и выводим второе

---
+++ HomeWork003/task003_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num / 10 > 0)
+{
+    Console.WriteLine($"Вторая цифра {num /10 % 10}");
+}
+else
+{
+    Console.WriteLine("Введено не корректное число");
+}
+
