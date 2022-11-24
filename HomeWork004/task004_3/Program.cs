173b9fddd91dc835806332ad58e59497e2844392
KseniaAk <ksu909090@yandex.ru>
2022-11-24 13:55:57 +0300

    29. массив из 8 элементов

---
+++ HomeWork004/task004_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+
+void FillArray(int[] arr)
+{
+    int length = arr.Length;
+    for(int i = 0; i<length; i++)
+    {
+        arr[i] = new Random().Next(8);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i<length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
