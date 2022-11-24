5cb34f65d76c7706f0027ca93f1c9efde88c0eb5
KseniaAk <ksu909090@yandex.ru>
2022-11-24 13:49:14 +0300

    27. сумма цифр в числе

---
+++ HomeWork004/task004_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 27.Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int FuncSum(int num)
+{
+    int sum = 0;
+    while(num>0)
+    {
+        int i = num % 10;
+        num = num / 10;
+        sum = sum + i;
+    }
+    return sum;
+}
+int sum = FuncSum(num);
+Console.Write(sum);
