a4ca9a77f9371dded7631b9261b10b3d8ba21c9b
KseniaAk <ksu909090@yandex.ru>
2022-11-25 09:39:46 +0300

    38. разница между максимальным и минимальным знначением  массива

---
+++ HomeWork005/task5_3/Program.cs
@@ -0,0 +1,46 @@
+﻿// 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+// минимальным элементов массива.
+Console.Clear();
+
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+    for(int i=0; i<size; i++)
+    {
+        res[i] = Convert.ToDouble(new Random().Next(minValue,maxValue+1)) / 100;
+    }
+    return res;
+}
+
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+}
+
+double[] array = GetArray(5,1,100);
+PrintArray(array);
+Console.WriteLine();
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int a = 0; a < array.Length; a++)
+{
+    if (array[a] > max)
+        {
+            max = array[a];
+        }
+    if (array[a] < min)
+        {
+            min = array[a];
+        }
+}
+
+Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
