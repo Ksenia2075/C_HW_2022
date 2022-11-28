6d2d58924710a2d544db2a5d9fa42129b67ab655
KseniaAk <ksu909090@yandex.ru>
2022-11-28 16:29:59 +0300

    считаем числа больше 0

---
+++ HomeWork006/task6_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int CountNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+Console.Write("Введите числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = CountNum(array); 
+Console.WriteLine($"Количество чисел больше 0: {n}");
+
+
