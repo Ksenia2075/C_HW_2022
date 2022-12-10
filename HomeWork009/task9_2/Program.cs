df62375f571b4d481f953ebfa52e2603893458f2
KseniaAk <ksu909090@yandex.ru>
2022-12-10 18:11:07 +0300

    выводим сумму чисел от m до n через рекурсию

---
+++ HomeWork009/task9_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumNumMToN(m-1, n));
+
+// МЕТОД
+// находим сумму чисел в заданном промежутке
+int SumNumMToN(int m, int n)
+{
+    int result = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        result = m + SumNumMToN(m, n);
+        return result;
+    }
+}
+
+
