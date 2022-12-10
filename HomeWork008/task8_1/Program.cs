6dd1bd40387fdfdceb50053b5c1116a63df5ce7a
KseniaAk <ksu909090@yandex.ru>
2022-12-10 11:33:02 +0300

    элементы по убыванию в двумерном массиве

---
+++ HomeWork008/task8_1/Program.cs
@@ -0,0 +1,74 @@
+﻿// 54. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+Console.Write("Введите минимальное значение массива: ");
+int minValue = int.Parse(Console.ReadLine());
+Console.Write("Введите максимальное значение массива: ");
+int maxValue = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, minValue, maxValue);     // получаем массив через метод
+PrintArray(array);
+Console.WriteLine();
+SortToLower(array);
+PrintArray(array);                                              // печатаем исходный массив
+
+
+
+// METHODS
+int[,] GetArray(int m, int n, int minValue, int maxValue)     // метод. получаем двумерный массив
+{
+    int[,] result = new int[m,n];
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue);   // заполненный случайными числами
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
