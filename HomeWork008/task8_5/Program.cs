ef7608f5003f5bbb2be75557ecf43a31951f7424
KseniaAk <ksu909090@yandex.ru>
2022-12-10 17:02:11 +0300

    заполняем спирально массив

---
+++ HomeWork008/task8_5/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[,] inArray = new int[size, size];
+GetArraySpiral(inArray, size);
+PrintArray(inArray);
+
+// МЕТОДЫ
+//  Функция заполнения массива по спирали начиная с 10
+void GetArraySpiral(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 10;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+// печатаем двумерный массив
+void PrintArray(int[,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
