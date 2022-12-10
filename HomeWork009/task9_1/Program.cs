2e9fe408d9356826e4ab2da21baaecc0e33d7ea1
KseniaAk <ksu909090@yandex.ru>
2022-12-10 17:26:00 +0300

    выводим четные числа от n до 1 через рекурсию

---
+++ HomeWork009/task9_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2"
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+EvenNumToLow(number, count);
+
+void EvenNumToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        EvenNumToLow(n, count + 2);
+        Console.Write(count + " ");
+    }
+}
+
